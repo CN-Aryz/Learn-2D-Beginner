f78b3f1629e6e82ed65f441c2a86aada2e118779
Aryz <1939196667@qq.com>
2019-06-19 07:36:11 +0800

    第一次制作2d游戏

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+
+    public float speed = 5.0f;
+    Rigidbody2D rigidbody2d;
+    Animator animator;
+
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    float timer;
+    int direction = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        timer = changeTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+        Vector2 position = rigidbody2d.position;
+
+        if (vertical)
+        {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+            position.y = position.y + Time.deltaTime * speed * direction;
+        }
+        else
+        {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+            position.x = position.x + Time.deltaTime * speed * direction;
+        }
+
+        rigidbody2d.MovePosition(position);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
     }
 }
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    //移动速度
     public float SPEED = 5.0f;
 
+    //满血量
     public int maxHealth = 5;
+    //无敌时间
     public float timeInvincible = 2.0f;
 
+    //是否无敌
     bool isInvincible;
+    //无敌时间倒计时
     float invincibleTimer;
 
+    //当前生命
     public int health { get { return currentHealth; } }
     int currentHealth;
 
+    Vector2 lookDirection = new Vector2(1, 0);
 
     Rigidbody2D rigidbody2d;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +34,12 @@ public class RubyController : MonoBehaviour
 
         rigidbody2d = GetComponent<Rigidbody2D>();
 
+        animator = GetComponent<Animator>();
+
         currentHealth = maxHealth;
+
+
+
     }
 
     // Update is called once per frame
@@ -36,9 +49,25 @@ public class RubyController : MonoBehaviour
         //Debug.Log(horizontal);
         float vertical = Input.GetAxis("Vertical");
         //Debug.Log(vertical);
+
+        Vector2 move = new Vector2(horizontal,vertical);
+
+        Debug.Log(move);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y,0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        Debug.Log(lookDirection);
+        
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         Vector2 position = transform.position;
-        position.x = position.x + SPEED * horizontal*Time.deltaTime;
-        position.y = position.y + SPEED * vertical*Time.deltaTime;
+        position = position + SPEED * move *Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
 
@@ -59,6 +88,7 @@ public class RubyController : MonoBehaviour
             if (isInvincible) {
                 return;
             }
+            animator.SetTrigger("Hit");
             isInvincible = true;
             invincibleTimer = timeInvincible;
         }
